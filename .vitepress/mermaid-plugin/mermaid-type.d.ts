c8b5451d2bf31ab251e4fac43a4ce53319b1cff2
s-elo <zz321yy@qq.com>
2023-05-11 19:11:20 +0800

    fix: use async import for mermaid

--- .vitepress/mermaid-plugin/mermaid-type.d.ts
+++ .vitepress/mermaid-plugin/mermaid-type.d.ts
@@ -1,6 +1,9 @@
 declare module 'https://cdn.jsdelivr.net/npm/mermaid@10/dist/mermaid.esm.min.mjs' {
-  const initialize: (config: any) => void
-  const render: (id: string, code: string) => Promise<{ svg: string }>
+  const Mermaid: {
+    initialize: (config: any) => void
+    render: (id: string, code: string) => Promise<{ svg: string }>
+  }
+  export default Mermaid
 }
 
 interface Window {
--- .vitepress/mermaid-plugin/mermaid.ts
+++ .vitepress/mermaid-plugin/mermaid.ts
@@ -1,10 +1,20 @@
-import mermaid from 'https://cdn.jsdelivr.net/npm/mermaid@10/dist/mermaid.esm.min.mjs'
+let mermaid:
+  | undefined
+  | typeof import('https://cdn.jsdelivr.net/npm/mermaid@10/dist/mermaid.esm.min.mjs').default
 
 export const render = async (
   id: string,
   code: string,
   config: any
 ): Promise<string> => {
+  if (!mermaid) {
+    mermaid = (
+      await import(
+        'https://cdn.jsdelivr.net/npm/mermaid@10/dist/mermaid.esm.min.mjs'
+      )
+    ).default
+  }
+  console.log(mermaid)
   mermaid.initialize(config)
 
   const { svg } = await mermaid.render(id, code)
