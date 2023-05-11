36bc8a4cab6e8b1eff314cb6675a2298fd86963c
s-elo <zz321yy@qq.com>
2023-05-11 12:40:24 +0800

    chore: change font family

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -6,20 +6,34 @@ export default defineConfig({
   description: 'source code reading of vue3',
   srcDir: 'src',
   base: '/vue3-reading/',
+  head: [
+    [
+      'link',
+      {
+        rel: 'stylesheet',
+        href: 'https://gw.alipayobjects.com/os/k/font/lxgwwenkaiscreenr.css',
+        as: 'font',
+        async: 'true'
+      }
+    ]
+  ],
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav: [
       { text: 'Home', link: '/' },
-      { text: 'Examples', link: '/markdown-examples' }
+      { text: 'Read', link: '/start' }
     ],
 
     sidebar: [
       {
-        text: 'Examples',
+        text: 'Usage Examples',
         collapsed: true,
         items: [
-          { text: 'Markdown Examples', link: '/markdown-examples' },
-          { text: 'Runtime API Examples', link: '/api-examples' }
+          {
+            text: 'Markdown Examples',
+            link: '/usage-examples/markdown-examples'
+          },
+          { text: 'Runtime API Examples', link: '/usage-examples/api-examples' }
         ]
       }
     ],
