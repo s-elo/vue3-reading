356a02d73e5be936c45c655511fb8e4c2216d640
lichao <lichao@shopee.com>
2023-05-12 18:48:35 +0800

    feat: add comment section with utteranc

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -1,15 +1,11 @@
 // https://vitepress.dev/guide/custom-theme
-import { h } from 'vue'
-import Theme from 'vitepress/theme'
+import DefaultTheme from 'vitepress/theme'
+import Layout from './Layout.vue'
 import './style.css'
 
 export default {
-  ...Theme,
-  Layout: () => {
-    return h(Theme.Layout, null, {
-      // https://vitepress.dev/guide/extending-default-theme#layout-slots
-    })
-  },
+  ...DefaultTheme,
+  Layout,
   enhanceApp({ app, router, siteData }) {
     // ...
   }
--- components/Test.vue
+++
@@ -1,3 +0,0 @@
-<template>
-  <span>test component</span>
-</template>
