f8933a833f0dc6e1a951d183ccb09c00ab53b133
s-elo <zz321yy@qq.com>
2023-05-12 18:48:35 +0800

    feat: add comment section with utteranc

---
+++ .vitepress/theme/Layout.vue
@@ -0,0 +1,15 @@
+<script lang="ts" setup>
+import DefaultTheme from 'vitepress/theme'
+import Comment from '../../components/Comment.vue'
+
+const { Layout } = DefaultTheme
+</script>
+
+<template>
+  <Layout>
+    <template #doc-after>
+      <Comment />
+    </template>
+  </Layout>
+</template>
+<style></style>
---
+++ components/Comment.vue
@@ -0,0 +1,45 @@
+<script lang="ts" setup>
+import { onMounted } from 'vue'
+
+const loadComment = () => {
+  const isDark = document.documentElement.classList.contains('dark')
+
+  const scriptDom = document.createElement('script')
+  const utterancesConfig = {
+    src: 'https://utteranc.es/client.js',
+    repo: 's-elo/vue3-reading',
+    'issue-term': 'pathname',
+    label: 'Comment',
+    theme: isDark ? 'github-dark' : 'github-light',
+    crossorigin: 'anonymous'
+  }
+
+  for (const config of Object.keys(utterancesConfig)) {
+    scriptDom.setAttribute(config, utterancesConfig[config])
+  }
+
+  const commentContainer = document.getElementById('utterances')
+  if (!commentContainer) return
+
+  commentContainer.append(scriptDom)
+
+  scriptDom.addEventListener('load', () => {
+    if (commentContainer.children.length <= 1) return;
+    
+    const originalComment = commentContainer.firstElementChild
+    if (!originalComment) return
+
+    commentContainer.removeChild(originalComment)
+  })
+}
+
+onMounted(() => {
+  loadComment()
+  const observer = new MutationObserver(() => loadComment())
+  observer.observe(document.documentElement, { attributes: true })
+})
+</script>
+<template>
+  <div id="utterances"></div>
+</template>
+<style scoped lang="scss"></style>
