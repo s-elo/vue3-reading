91df89eddb9e8c38fc7d468155d168658c93dd17
s-elo <zz321yy@qq.com>
2023-05-14 08:01:26 +0800

    feat: add Modal and larger mermaid

---
+++ components/Modal.vue
@@ -0,0 +1,64 @@
+<script lang="ts" setup>
+const props = defineProps<{ modelValue: boolean; closeByClickBackground?: boolean;}>()
+const emits = defineEmits(['update:modelValue'])
+
+const clickMask = () => {
+  if (props.closeByClickBackground === true) {
+    emits('update:modelValue')
+  }
+}
+</script>
+
+<template>
+  <Teleport to="body">
+    <Transition name="modal">
+      <div v-show="modelValue" class="modal-mask" @click="clickMask">
+        <div class="modal-container">
+          <slot></slot>
+        </div>
+      </div>
+    </Transition>
+  </Teleport>
+</template>
+
+<style lang="scss" scoped>
+.modal-mask {
+  position: fixed;
+  z-index: 200;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  transition: opacity 0.3s ease;
+}
+
+.modal-container {
+  width: fit-content;
+  margin: auto;
+  padding: 20px 30px;
+  background-color: var(--vp-c-bg);
+  border-radius: 2px;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.33);
+  transition: all 0.3s ease;
+}
+
+.modal-button:hover {
+  border-color: var(--vp-button-alt-hover-border);
+  color: var(--vp-button-alt-hover-text);
+  background-color: var(--vp-button-alt-hover-bg);
+}
+
+.modal-enter-from,
+.modal-leave-to {
+  opacity: 0;
+}
+
+.modal-enter-from .modal-container,
+.modal-leave-to .modal-container {
+  transform: scale(1.1);
+}
+</style>
